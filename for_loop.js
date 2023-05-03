9759a88af43e69e123a57c6ba1e22e9d0c1b41ae
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-03 23:53:32 +0600

    for loop done

---
+++ for_loop.js
@@ -0,0 +1,51 @@
+
+//  for loop
+for (let i = 0; i <= 10; i++) {
+
+    console.log(i)
+}
+
+/* ans :-
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/ 
+
+
+// even number..
+for (let i = 0; i <= 10; i+=2) {
+    
+    console.log(i)
+}
+
+/* ans :-
+0
+2
+4
+6
+8
+10
+*/ 
+
+
+// odd number..
+for (let i = 1; i <= 10; i+=2) {
+    
+    console.log(i)
+    
+}
+
+/* ans :-
+1
+3
+5
+7
+9
+*/ 
