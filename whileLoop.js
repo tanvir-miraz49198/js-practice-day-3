286a2c440b4316d489aee4e15455dba9bfea4593
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-03 23:42:19 +0600

    while loop add

---
+++ whileLoop.js
@@ -0,0 +1,18 @@
+// while..
+
+var num = 0;
+while (num < 7) {
+   console.log( "i am a good boy")
+
+   num++;
+}
+*/
+/* ans :-
+i am a good boy
+i am a good boy
+i am a good boy
+i am a good boy
+i am a good boy
+i am a good boy
+i am a good boy
+*/ 
