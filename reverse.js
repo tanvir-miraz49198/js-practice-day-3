6fd3705a832285b5820643d90ab1e2242f44d675
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-04 00:02:23 +0600

    reverse practice done

---
+++ reverse.js
@@ -0,0 +1,41 @@
+// reverse
+for(var i = 10; i >=1; i--){
+
+    console.log(i)
+  }
+  /* ans :-
+  10
+  9
+  8
+  7
+  6
+  5
+  4
+  3
+  2
+  1
+  */ 
+  
+  var i = 10;
+  while(i >= 1){
+   console.log(i)
+  
+   i--;
+     
+  }
+  
+  /* ans :-
+  10
+  9
+  8
+  7
+  6
+  5
+  4
+  3
+  2
+  1
+  */ 
+  
+  
+  
