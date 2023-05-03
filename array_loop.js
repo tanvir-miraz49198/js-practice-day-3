89c62573f0120fc597c65fa8d59dbe8a031e78ff
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-03 23:55:11 +0600

    array loop done

---
+++ array_loop.js
@@ -0,0 +1,37 @@
+// array loop
+
+var numbers = [10, 20, 30, 90, 120, 150]
+
+for (let i = 0; i < numbers.length; i++) {
+
+    const element = numbers[i];
+    console.log(element)
+}
+
+/* ans :-
+10
+20
+30
+90
+120
+150
+*/ 
+
+
+var friendNames = ["Tanvir", "sabit", "susan", "Susan", "aqib"]
+
+for (let i = 0; i < friendNames.length; i++) {
+
+    const element = friendNames[i];
+
+    console.log(element)
+}
+
+/*ans :-
+Tanvir
+sabit
+susan
+Susan
+aqib
+*/ 
+
