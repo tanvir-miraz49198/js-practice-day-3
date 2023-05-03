523e1cc326bfae13188284b20d3a4a5432afe892
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-03 23:51:35 +0600

    while loop 2nd try done

---
+++ whileLoop_2ndPart.js
@@ -0,0 +1,36 @@
+// while (2nd try)
+
+// even number..
+var number = 2;
+while (number <= 10) {
+
+    console.log(number);
+
+    number = number + 2;
+}
+
+/* ans : -
+0
+2
+4
+6
+8
+*/ 
+
+
+// odd number..
+var number = 1;
+while (number <= 10) {
+    console.log(number);
+
+    number = number + 2;
+
+}
+
+/* ans :-
+1
+3
+5
+7
+9
+*/ 
